62d6d6125c1d49cdd8cacbf9d4f44525f2505ea1
lazim2142 <lazim2142@hotmail.com>
2017-03-04 02:08:52 -0500

    PCL filtering working.

--- src/obstacle_detector.cpp
+++ src/obstacle_detector.cpp
@@ -3,6 +3,7 @@
 #include <pcl_ros/point_cloud.h>
 #include <pcl/kdtree/kdtree_flann.h>
 #include <pcl/filters/passthrough.h>
+#include <pcl/filters/statistical_outlier_removal.h>
 #include <tf/transform_listener.h>
 
 ros::Publisher vis_pub;
@@ -11,29 +12,39 @@ tf::TransformListener* tf_sub = NULL;
 void pointCloudCallback(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr& msg)
 {
     // Transform point cloud to world frame
-    pcl::PointCloud<pcl::PointXYZ>::Ptr transformed_pcl(new pcl::PointCloud<pcl::PointXYZ>);
-    pcl_ros::transformPointCloud("/world", *msg, *transformed_pcl, *tf_sub);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+    tf_sub->waitForTransform("/stereo_camera", "/world", ros::Time::now(), ros::Duration(1.0));
+    pcl_ros::transformPointCloud("/world", *msg, *cloud, *tf_sub);
 
-    // Filter out points low to the ground
-    pcl::PassThrough<pcl::PointXYZ> low_filter;
-    low_filter.setInputCloud(transformed_pcl);
-    low_filter.setFilterFieldName("z");
-    low_filter.setFilterLimits(-1.0, 1.0);
+    // Filter out points low to the ground and above 2m from the ground plane
+    pcl::PassThrough<pcl::PointXYZ> filter;
+    filter.setInputCloud(cloud);
+    filter.setFilterFieldName("z");
+    filter.setFilterLimits(0.1016, 2.0);
+    filter.filter(*cloud);
 
-    pcl::PointCloud<pcl::PointXYZ>::Ptr high_cloud(new pcl::PointCloud<pcl::PointXYZ>);
-    low_filter.filter(*high_cloud);
-    vis_pub.publish(high_cloud);
+    // Filter out points more than 3 meters away
+    filter.setFilterFieldName("y");
+    filter.setFilterLimits(0, 3.0);
+    filter.filter(*cloud);
 
-    if (high_cloud->points.size() > 0)
+    if (cloud->width > 25)
     {
+        // Filter Noise
+        pcl::StatisticalOutlierRemoval<pcl::PointXYZ> sor;
+        sor.setInputCloud (cloud);
+        sor.setMeanK (100);
+        sor.setStddevMulThresh (1.0);
+        sor.filter (*cloud);
+
         pcl::PointXYZ origin;
         origin.x = 0.0f;
         origin.y = 0.0f;
         origin.z = 0.0f;
-        int K = 10;
+        int K = 1;
 
         pcl::KdTreeFLANN<pcl::PointXYZ> kdtree;
-        kdtree.setInputCloud(high_cloud);
+        kdtree.setInputCloud(cloud);
         std::vector<int> pointIdxNKNSearch(K);
         std::vector<float> pointNKNSquaredDistance(K);
 
@@ -45,12 +56,12 @@ void pointCloudCallback(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr& msg)
         if (kdtree.nearestKSearch(origin, K, pointIdxNKNSearch, pointNKNSquaredDistance) > 0 )
         {
             for (size_t i = 0; i < pointIdxNKNSearch.size(); ++i)
-                nneighbors.points.push_back(pcl::PointXYZ(high_cloud->points[ pointIdxNKNSearch[i] ].x,
-                                                            high_cloud->points[ pointIdxNKNSearch[i] ].y,
-                                                            high_cloud->points[ pointIdxNKNSearch[i] ].z));
+                nneighbors.points.push_back(pcl::PointXYZ(cloud->points[ pointIdxNKNSearch[i] ].x,
+                                                            cloud->points[ pointIdxNKNSearch[i] ].y,
+                                                            cloud->points[ pointIdxNKNSearch[i] ].z));
         }
 
-        //vis_pub.publish(nneighbors.makeShared());
+        vis_pub.publish(nneighbors.makeShared());
     }
 }
 
