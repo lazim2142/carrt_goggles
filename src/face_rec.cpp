ed51812f0b13aee02bb425de470c1268baa0a0d6
lazim2142 <lazim2142@hotmail.com>
2017-04-19 13:24:53 -0400

    Fixed a bug with facerecognition where grayscale images were being saved as 3 channel

--- src/face_rec.cpp
+++ src/face_rec.cpp
@@ -45,9 +45,29 @@ void trainCallback(const sensor_msgs::ImageConstPtr& image){
     }
 }
 
-cv::Ptr<cv::face::FaceRecognizer> model;
+static cv::Mat norm_0_255(cv::InputArray _src) {
+    cv::Mat src = _src.getMat();
+    // Create and return normalized image:
+    cv::Mat dst;
+    switch(src.channels()) {
+    case 1:
+        cv::normalize(_src, dst, 0, 255, cv::NORM_MINMAX, CV_8UC1);
+        break;
+    case 3:
+        cv::normalize(_src, dst, 0, 255, cv::NORM_MINMAX, CV_8UC3);
+        break;
+    default:
+        src.copyTo(dst);
+        break;
+    }
+    return dst;
+}
+
+
+cv::Ptr<cv::face::BasicFaceRecognizer> model;
 std::map<int, std::string> hash_name_dict;
 ros::Publisher detection_pub;
+bool visualize = false;
 void recCallback(const sensor_msgs::ImageConstPtr& image) {
     cv_bridge::CvImagePtr cv_img = cv_bridge::toCvCopy(image, "bgr8");
     cv::Mat gray;
@@ -55,18 +75,35 @@ void recCallback(const sensor_msgs::ImageConstPtr& image) {
 
     std::vector<cv::Rect_<int> > faces;
     haar_cascade.detectMultiScale(gray, faces);
-
+    cv::Mat mean;
     for(int i = 0; i < faces.size(); i++) {
         cv::Rect face_i = faces[i];
         cv::Mat face = gray(face_i);
         cv::resize(face, face, cv::Size(IM_WIDTH, IM_HEIGHT), 1.0, 1.0, cv::INTER_CUBIC);
-        int prediction; double confidence;
+        int prediction = -1; double confidence;
         model->predict(face, prediction, confidence);
 
-        std_msgs::String recognized_face;
-        recognized_face.data = "Found " + hash_name_dict[prediction];
-        detection_pub.publish(recognized_face);
-        ROS_INFO("%s with confidence %f", recognized_face.data.c_str(), (float)confidence);
+        if(prediction >= 0) {
+            std_msgs::String recognized_face;
+            recognized_face.data = "Found " + hash_name_dict[prediction];
+            detection_pub.publish(recognized_face);
+            //ROS_INFO("%s with confidence %f", recognized_face.data.c_str(), (float)confidence);
+
+            if(visualize){
+                cv::rectangle(gray, face_i, CV_RGB(0, 255,0), 1);
+                std::string box_text = cv::format("%s, %f", hash_name_dict[prediction].c_str(), confidence);
+                int pos_x = std::max(face_i.tl().x - 10, 0);
+                int pos_y = std::max(face_i.tl().y - 10, 0);
+                cv::putText(gray, box_text, cv::Point(pos_x, pos_y), cv::FONT_HERSHEY_PLAIN, 1.0, CV_RGB(0,255,0), 2.0);
+
+                mean = model->getMean();
+            }
+        }
+    }
+
+    if(visualize) {
+        cv::imshow("face_recognizer", gray);
+        cv::waitKey(3);
     }
 }
 
@@ -83,14 +120,16 @@ void loadImagesAndLabels(std::vector<cv::Mat>& images, std::vector<int>& labels)
     boost::filesystem::directory_iterator end_itr;
     for (boost::filesystem::directory_iterator itr(image_dir); itr != end_itr; ++itr) {
         std::string image_path = image_dir + itr->path().filename().string();
-        cv::Mat image = cv::imread(image_path);
+        cv::Mat image = cv::imread(image_path, CV_8U);
         images.push_back(image);
 
         std::size_t underscore_index = itr->path().filename().string().find("_");
         std::string person_name = itr->path().filename().string().substr(0, underscore_index);
-        std::vector<unsigned char> c_vec(person_name.begin(), person_name.end());
+        std::vector<unsigned char> c_vec(person_name.begin(), person_name.end());        
         c_vec.push_back(0);
         labels.push_back(hash(&c_vec[0]));
+
+        //ROS_INFO("Hashing %s to %d", itr->path().filename().string().c_str(), hash(&c_vec[0]));
     }
 }
 
@@ -101,6 +140,7 @@ void initHash2NameDictionary(){
         std::size_t underscore_index = itr->path().filename().string().find("_");
         std::string person_name = itr->path().filename().string().substr(0, underscore_index);
         std::vector<unsigned char> c_vec(person_name.begin(), person_name.end());
+        //ROS_INFO("Hashing %s to %d", person_name.c_str(), hash(&c_vec[0]));
         hash_name_dict[hash(&c_vec[0])] = person_name;
     }
 }
@@ -112,18 +152,22 @@ int main(int argc, char **argv) {
     ROS_INFO("Loading HAAR Cascade from: %s", haar_cascade_path.c_str());
     haar_cascade.load(haar_cascade_path);
 
+    ros::init(argc, argv, "face_rec");
+    ros::NodeHandle nh("~");
     name = "";
-    if (argc == 2 || argc == 4)
-        name = argv[1];
+    if(!nh.getParam("cmd", name)) {
+        ROS_INFO("Please pass in param cmd, possible values = recognize, train, name_of_person");
+        return 0;
+    }
+    nh.getParam("visualize", visualize);
 
     if(name.compare("recognize") == 0 || name.compare("Recognize") == 0){
         ROS_INFO("Recognizing");
         initHash2NameDictionary();
+
         model = cv::face::createFisherFaceRecognizer();
         model->load(fisher_model_path);
 
-        ros::init(argc, argv, "face_rec");
-        ros::NodeHandle nh;
         detection_pub = nh.advertise<std_msgs::String>("recognized_faces", 1);
         ros::Subscriber image_sub = nh.subscribe("image_raw", 1, recCallback);
         ros::spin();
@@ -132,6 +176,7 @@ int main(int argc, char **argv) {
         ROS_INFO("Training");
         std::vector<cv::Mat> images;
         std::vector<int> labels;
+
         loadImagesAndLabels(images, labels);
         model = cv::face::createFisherFaceRecognizer();
         model->train(images, labels);
@@ -139,8 +184,6 @@ int main(int argc, char **argv) {
     }
     else if (name.length() > 0){
         ROS_INFO("Capturing Training Images");
-        ros::init(argc, argv, "face_rec_train");
-        ros::NodeHandle nh;
         ros::Subscriber image_sub = nh.subscribe("image_raw", 1, trainCallback);
         ros::spin();
     }
